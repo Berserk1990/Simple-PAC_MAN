6c560387f1b20d33a487648ab49428c2cdff2206
student <student@STEP.LAN>
2016-09-19 21:40:39 +0300

    Game1

---
+++ Simple PACMAN/Assets/Code/CellComponent.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class CellComponent : MonoBehaviour {
+
+	public event Action<int, int> OnItemClick;
+
+	private void OnItemClickHandler(){
+		if (OnItemClick != null)
+			OnItemClick (Row, Column);
+	}
+
+	public int Row {
+		set;
+		get;
+	}
+	public int Column {
+		set;
+		get;
+	}
+
+	[SerializeField]
+	private Image _image;
+
+	[SerializeField]
+	private Color _selectColor;
+
+	private Color _normalColor;
+	private float _delay = .25f;
+
+
+	private bool _isSelect;
+	public bool IsSelect{
+		set{ 
+			_isSelect = value;
+			CancelInvoke ();
+			if (value)
+				InvokeRepeating ("SwichColor", 0, _delay);
+			else
+				_image.color = _normalColor;
+		}
+		get{ 
+			return _isSelect;
+		}
+	}
+
+	public void Selected(){
+		IsSelect = !IsSelect;
+	}
+
+	private void Start(){
+		if (_image)
+			_normalColor = _image.color;
+	}
+
+	private void SwichColor(){
+		if (!_image)
+			return;
+
+		_image.color = _image.color == _normalColor ? _selectColor : _normalColor;
+	}
+
+	public void OnSelected(){
+		OnItemClickHandler ();
+	}
+}
---
+++ Simple PACMAN/Assets/Code/GameController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Game.Data;
+using System.Collections.Generic;
+using System;
+
+public class GameController : MonoBehaviour {
+
+
+	[SerializeField]
+	private CellComponent _prefab;
+
+	[SerializeField]
+	private Transform _wrapper;
+
+	private int _rowCount = 10;
+	private int _columnCount = 10;
+	private CellComponent[,] _field;
+
+	private int _previousSelectedRow;
+	private int _previousSelectedColumn;
+
+	private void GenerateField(){
+		if (!_prefab || !_wrapper)
+			return;
+
+		_field = new CellComponent[_rowCount, _columnCount];
+
+		for (int i = 0; i < _rowCount; i++) {
+			for (int j = 0; j < _columnCount; j++) {		
+			
+				var item = _prefab.GetInstance ();
+				item.transform.SetParent (_wrapper, false);
+				_field [i, j] = item;
+				item.gameObject.name = string.Format ("{0} , {1}", i, j);
+				item.Row = i;
+				item.Column = j;
+				item.OnItemClick += Item_OnItemClick;
+			}
+		}
+	}
+
+	void Item_OnItemClick (int row, int column)
+	{
+		Debug.LogFormat ("{0} , {1}", row, column);
+		_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = false;
+		_field [row, column].IsSelect = true;
+		_previousSelectedRow = row;
+		_previousSelectedColumn = column;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GenerateField ();
+	}
+
+	private void LateUpdate(){
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = false;
+			_previousSelectedRow--;
+			_previousSelectedRow = _previousSelectedRow < 0 ? _field.GetLength(0) - 1 : _previousSelectedRow;
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = true;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = false;
+			_previousSelectedRow++;
+			_previousSelectedRow = _previousSelectedRow >= _field.GetLength(0) ? 0 : _previousSelectedRow;
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = true;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = false;
+			_previousSelectedColumn++;
+			_previousSelectedColumn = _previousSelectedColumn >= _field.GetLength(1) ? 0 : _previousSelectedColumn;
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = true;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = false;
+			_previousSelectedColumn--;
+			_previousSelectedColumn = _previousSelectedColumn < 0 ? _field.GetLength(1) - 1 : _previousSelectedColumn;
+			_field [_previousSelectedRow, _previousSelectedColumn].IsSelect = true;
+		}
+	}
+}
